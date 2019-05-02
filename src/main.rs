d9bbdf4a4fa2e07f7cbde060d597138d0ac52a7b
Kent Inverarity <kinverarity@hotmail.com>
2019-05-02 21:51:32 +0930

    turn the input into a function

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,19 @@ fn main() {
     println!("Archie's law: porosity = (a / F) ^ (1 / m)");
     println!("Formation factor F = Ro / Rw");
 
-    println!("Ro? ");
-    let mut ro = String::new();
-    io::stdin()
-        .read_line(&mut ro)
-        .expect("Please enter something");
-    let ro: f64 = ro.trim().parse().expect("Please enter a number");
-
-    println!("Rw? ");
-    let mut rw = String::new();
-    io::stdin().read_line(&mut rw).expect("Please enter something");
-    let rw: f64 = rw.trim().parse().expect("Please enter a number");
-
+    let ro = get_param("Ro".to_string());
+    let rw = get_param("Rw".to_string());
     println!("Formation factor = {}", ro / rw);
+    let a = get_param("a".to_string());
+    let m = get_param("m".to_string());
+
+    let porosity = (a / (ro / rw)).powf(1. / m);
+    println!("-> porosity {}", porosity * 100.);
+}
+
+fn get_param(label: String) -> f64 {
+    let mut line = String::new();
+    println!("{}?", label);
+    io::stdin().read_line(&mut line).expect("Please enter a value");
+    line.trim().parse().expect("Please enter a floating point number")
 }
