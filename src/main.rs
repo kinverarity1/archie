3c912eadaa0446aeefa629b23fbf60e96a5b792a
Kent Inverarity <kinverarity@hotmail.com>
2019-05-02 22:04:41 +0930

    update function parameter to &str

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@ fn main() {
     println!("Archie's law: porosity = (a / F) ^ (1 / m)");
     println!("Formation factor F = Ro / Rw");
 
-    let ro = get_param("Ro".to_string());
-    let rw = get_param("Rw".to_string());
+    let ro = get_param("Ro");
+    let rw = get_param("Rw");
     println!("Formation factor = {}", ro / rw);
-    let a = get_param("a".to_string());
-    let m = get_param("m".to_string());
+    let a = get_param("a");
+    let m = get_param("m");
 
     let porosity = (a / (ro / rw)).powf(1. / m);
     println!("-> porosity {}", porosity * 100.);
 }
 
-fn get_param(label: String) -> f64 {
+fn get_param(label: &str) -> f64 {
     let mut line = String::new();
     println!("{}?", label);
     io::stdin().read_line(&mut line).expect("Please enter a value");
